987b23185761a64e51667802ca7c26f7e6060154
Bryan <bjh7242@rit.edu>
2015-11-10 22:45:07 -0500

    One liner script to get the average rate of data through a pipe

---
+++ pipe.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# this script measures how long it takes to push 512 bytes through a pipe
+# pv must be installed
+
+# The average data rate to make a 51MB file will be displayed
+dd if=/dev/urandom bs=512 count=100000 | pv -a | dd of=file
+
