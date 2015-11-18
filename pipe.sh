99338288f3102d56c84bb6093c7f6a959710f113
Bryan <bjh7242@rit.edu>
2015-11-18 01:11:21 -0500

    Suppressed output of dd

--- pipe.sh
+++ pipe.sh
@@ -3,5 +3,5 @@
 # pv must be installed
 
 # The average data rate to make a 51MB file will be displayed
-dd if=/dev/urandom bs=512 count=100000 | pv -a | dd of=file
+dd if=/dev/urandom bs=512 count=100000 status=none | pv -a | dd of=file status=none
 
